2d05dbbcee1fb063be1c40c6ffe40492a55716f0
bgunwani <bhawna.gunwani@gmail.com>
2021-06-04 12:49:22 +0530

    Session Three Commit

---
+++ SessionThree_WebApplication_Projects/coreWebApplication/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using coreWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coreWebApplication.Controllers
+{
+    public class CustomerController : Controller
+    {
+        IList<Customer> customerList = new List<Customer>() {
+            new Customer() { Id=1, Name="King", Amount=12000},
+            new Customer() { Id=2, Name="Kochhar", Amount=22000},
+            new Customer() { Id=3, Name="Shreya", Amount=32000},
+            new Customer() { Id=4, Name="Gautam", Amount=40000},
+            new Customer() { Id=5, Name="Kartik", Amount=22000}
+        };
+        
+        public ViewResult Index()
+        {
+            ViewBag.message = "Customer Management System";
+            ViewBag.customers = customerList;
+            return View();
+        }
+    }
+}
---
+++ SessionThree_WebApplication_Projects/coreWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coreWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewBag.Title = "Index Page";
+            return View();
+        }
+        public ViewResult About()
+        {
+            ViewBag.Title = "About Page";
+            return View("AboutPage");
+        }
+    }
+}
